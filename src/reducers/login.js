10c270b1413fd0e42f0b4864ad1637ece798ad21
Boris Civcic <civcicboris93@gmail.com>
2019-06-23 09:01:56 +0200

    add: console.log

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -3,6 +3,7 @@ import * as constants from '../actions/constants'
 const login = (state = 0, action) => {
   switch (action.type) {
     case constants.LOGIN_USER:
+      console.log('aaa', action.data)
       return action.userData
     default:
       return state
--- src/reducers/register.js
+++ src/reducers/register.js
@@ -4,7 +4,6 @@ import * as constants from '../actions/constants'
 const register = (state = 0, action) => {
   switch (action.type) {
     case constants.REGISTER_USER:
-      console.log('aaa', action.data)
       return action.data
     default:
       return state
